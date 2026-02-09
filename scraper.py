c81ca1f42e7249d2f63ae73d6761f13742490500
md-danish-lg <dk665226@gmail.com>
2026-02-09 13:37:31 +0500

    added multipage scraping, custom headers and error handling

--- scraper.py
+++ scraper.py
@@ -1,38 +1,70 @@
 import pandas as pd
 import requests
+import time
 from bs4 import BeautifulSoup
 
 
 
 
-site = requests.get("https://books.toscrape.com/")
+
+headers ={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"}
+
+book_details = []
+book_boxes = []
+amount_of_books = 0
+for i in range(1,4):
+    site_url = f"https://books.toscrape.com/catalogue/page-{i}.html"
 
 
-if site.status_code == 200:
-    soup = BeautifulSoup(site.text, 'html.parser')
+    print(f"Scraping Page {i}")
+    site = requests.get(site_url, headers=headers)
 
-    book_boxes = []
-    book_boxes = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
-    
 
-    print("Scraped "+ str(len(book_boxes))+" books")
-
-    book_details = []
-
-    for book in book_boxes:
-        price = book.find("div", class_="product_price")
+    if site.status_code != 200:
+        print("Error Accessing Website")
+        break
         
+    else:
+        soup = BeautifulSoup(site.text, 'html.parser')
+
         
+        book_boxes = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
         
-        book_details.append({"name": book.a.img['alt'],
-                                "price": price.p.text,
-                                "product_link": "https://books.toscrape.com/" + book.a['href']})
+
+        amount_of_books += len(book_boxes)
+        
+
+        
+
+        for book in book_boxes:
+            if book.find("div", class_="product_price"):
+                price = book.find("div", class_="product_price").p.text
+            else:
+                price = "price Missing"
+            
+
+            if book.a.img['alt']:
+                name =  book.a.img['alt']
+            else:
+                name = "Name Missing"
+
+            if book.a['href']:
+                link = "https://books.toscrape.com/" + book.a['href']
+            else:
+                link = "Link Missing"
+            
+            
+            book_details.append({"name":name,
+                                    "price": price,
+                                    "product_link": link})
+    time.sleep(1)
 
 
-    print("Extracted Books from the site!")
-    data = pd.DataFrame(book_details)
-    data.to_csv("books.csv")
-    data.to_excel("books.xlsx", index=False)
-    print("Books saved to the Files")
-else:
-    print("Error Accessing Website")
+print("Scraped "+ str(amount_of_books)+" books")
+print("Extracted Books from the site!")
+data = pd.DataFrame(book_details)
+data.to_csv("books.csv")
+data.to_excel("books.xlsx", index=False)
+print("Books saved to the Files")
+
+            
